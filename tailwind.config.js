3a91a76883e08aac9ce66c539d7ca577e7633131
paul-emas <pjobiggi@gmail.com>
2022-01-31 09:43:40 +0100

    new changes to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     screens: {
       'home-h': { raw: '(min-height: 900px)' },
