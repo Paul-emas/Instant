5d0640a1246f6d4d2e30635fa61485ff3fef058f
paul-emas <pjobiggi@gmail.com>
2022-02-02 12:26:05 +0100

    added back an overflow to header component

--- components/homepage/Header.jsx
+++ components/homepage/Header.jsx
@@ -3,7 +3,11 @@ import Navbar from '../layout/Navbar';
 const Header = ({ children, bg }) => (
   <>
     <Navbar />
-    <header className={`${!bg ? 'bg-primary-light' : 'banner'}  pb-10 lg:pb-0`}>
+    <header
+      className={`${
+        !bg ? 'bg-primary-light' : 'banner'
+      } pb-10  lg:overflow-hidden lg:pb-0`}
+    >
       <div className="mx-auto px-3 lg:h-banner xl:container xl:px-14">
         <div className="w-full">{children}</div>
       </div>
