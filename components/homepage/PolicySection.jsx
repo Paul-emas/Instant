c725dc6a499581550df7e22d5c802b80ec47dbd3
paul-emas <pjobiggi@gmail.com>
2022-03-08 11:33:58 +0100

    project files

--- components/homepage/PolicySection.jsx
+++ components/homepage/PolicySection.jsx
@@ -2,22 +2,35 @@ import { useEffect } from 'react';
 import gsap from 'gsap';
 import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import { useState } from 'react';
 
 const PolicySection = () => {
+  const [isFixed, setIsFixed] = useState(false);
   useEffect(() => {
-    gsap.to('.static-nav', {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: '.static-nav',
+        endTrigger: '.last-p',
         start: 'top top',
+        end: 'top top',
         pin: true,
+        pinSpacing: false,
+        onEnter: () => setIsFixed(true),
+        onEnterBack: () => setIsFixed(true),
+        onLeave: () => setIsFixed(false),
+        onLeaveBack: () => setIsFixed(false),
       },
     });
   }, []);
 
+  console.log(isFixed);
+
   return (
-    <div className="mx-auto px-4 xl:container xl:px-14">
+    <div className="terms mx-auto overflow-hidden px-4 xl:container xl:px-14">
       <div className="flex py-36">
-        <div className="static-nav w-72">
+        <div className={`static-nav w-72 ${isFixed ? 'static-padding' : ''}`}>
           <div>
             <div className="flex w-full cursor-pointer items-center py-3">
               <FontAwesomeIcon icon={faChevronCircleDown} className="h-5 w-5 text-primary-base" />
@@ -35,9 +48,6 @@ const PolicySection = () => {
               </button>
               <button className="w-full text-left text-gray-400 hover:text-primary-base">
                 Sub Link 4
-              </button>
-              <button className="w-full text-left text-gray-400 hover:text-primary-base">
-                Sub Link 5
               </button>
             </div>
           </div>
@@ -118,7 +128,7 @@ const PolicySection = () => {
               fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick
               zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How
             </div>
-            <div className="my-5 text-justify leading-loose">
+            <div className="last-p my-5 text-justify leading-loose">
               The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk
               MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph,
               for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt
