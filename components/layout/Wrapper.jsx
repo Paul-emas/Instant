20d111044aaa43e5e3dcb07fc4217c74d37b66e0
paul-emas <pjobiggi@gmail.com>
2022-04-03 08:30:44 +0100

    updated date to 5th of April

--- components/layout/Wrapper.jsx
+++ components/layout/Wrapper.jsx
@@ -112,7 +112,7 @@ const Wrapper = ({ children }) => {
                           <FontAwesomeIcon icon={faExclamationTriangle} className="hidden h-4 w-4 text-sm lg:block" />
                           <span className="ml-1">
                             Welcome to the new Instant Energy website. Transactions will resume on{' '}
-                            <strong>1st April 2022 </strong>
+                            <strong>5th April 2022 </strong>
                           </span>
                         </div>
                       </div>
