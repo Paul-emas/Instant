71fc4bf8dab7e9d928f79f2c88491be0ce9152c1
paul-emas <pjobiggi@gmail.com>
2022-04-05 12:42:08 +0100

    removed quickbuy disabled state

--- components/BuyAdsCard.jsx
+++ components/BuyAdsCard.jsx
@@ -10,39 +10,7 @@ const BuyAdsCard = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="h-224 w-full items-center rounded-xl bg-secondary-teal py-7 px-5 lg:flex lg:w-1/2 lg:p-4">
-      <div className="hidden lg:block">
-        <Image
-          src="/images/ads.jpg"
-          width={178}
-          height={192}
-          objectFit="cover"
-          objectPosition="right"
-          className="rounded-xl"
-        />
-      </div>
-      <div className="xl:ml-6">
-        <div className="text-xl font-bold text-white lg:text-2xl">
-          Start saving more on <br /> electricity
-        </div>
-        <p className="max-w-md text-sm text-gray-200 lg:text-base">
-          Earn and save more when you create an{' '}
-          <br className="hidden lg:block" /> account with us.
-        </p>
-        <div className="mt-10 w-full lg:mt-6">
-          <Button
-            className="w-full lg:w-auto"
-            onClick={() => dispatch(setInitAuthentication('register'))}
-            white
-          >
-            <div className="flex items-center justify-center">
-              <span>Start Saving now</span>
-              <FontAwesomeIcon icon={faChevronRight} className="ml-2 h-4 w-4" />
-            </div>
-          </Button>
-        </div>
-      </div>
-    </div>
+    <div className="h-224 w-full items-center rounded-xl bg-secondary-teal py-7 px-5 lg:flex lg:w-1/2 lg:p-4"></div>
   );
 };
 
