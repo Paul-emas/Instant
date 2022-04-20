1818308fcb6ddb7dc478eda3dd20409a1521d69b
paul-emas <pjobiggi@gmail.com>
2022-04-20 22:03:10 +0100

    ui updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  env: {
-    IEAPI_URL: 'https://live.ieapis.com/v1',
-    PAYSTACK_PUB_KEY: 'pk_live_f9dd6908a0c8515502da8cbcda4cdc1fc1fa06ba',
-    GOOGLE_ANALYTICS: 'G-74MVE3VVN1',
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
